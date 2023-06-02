e6d3d23b59789a9522f44c0281b0ba352d07927b
Gaius Nale <135344792+GaiusNale@users.noreply.github.com>
2023-06-02 18:58:28 +0100

    Add files via upload

---
+++ TIIDELAB Mini Project/js/script.js
@@ -0,0 +1,40 @@
+
+// Get elements from the DOM
+const form = document.getElementById('form');
+const btn = document.getElementById('submitBtn');
+const firstName = document.getElementById('firstName');
+const middleName = document.getElementById('middleName');
+const lastName = document.getElementById('lastName');
+const dob = document.getElementById('dob');
+const address = document.getElementById('address');
+const studentID = document.getElementById('studentID');
+const city = document.getElementById('city');
+const state = document.getElementById('state');
+const country = document.getElementById('country');
+const zipCode = document.getElementById('zipCode');
+const email = document.getElementById('email');
+const comments = document.getElementById('comments');
+
+// Add eventlistener to the form
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const data = {
+    firstName: firstName.value,
+    middleName: middleName.value,
+    lastName: lastName.value,
+    dob: dob.value,
+    address: address.value,
+    city: city.value,
+    studentID: studentID.value,
+    state: state.value,
+    country: country.value,
+    zipCode: zipCode.value,
+    email: email.value,
+    comments: comments.value,
+  }
+
+  sessionStorage.setItem("user", JSON.stringify(data))
+   // Redirect to another page
+    window.location.assign('preview.html')
+})
